c10ab6dd692b2b8b66ff25787aaf09abcbadc97e
Olof Wallentin <olov.wallentin@gmail.com>
2015-07-23 00:37:49 +0900

    Extended and cleaned GFXDevice
    Adds member functions for rendering geometric shapes and simplified
    rendering brushes. Now uses one brush for everything.

--- Projects/Engine/Graphics/GFXDevice.cpp
+++ Projects/Engine/Graphics/GFXDevice.cpp
@@ -1,6 +1,7 @@
 //	GFXDevice
 
 #include "GFXDevice.h"
+#include "Color.h"
 
 namespace neko
 {
@@ -9,8 +10,8 @@ GFXDevice::GFXDevice()
 {
 	m_pFactory = NULL;
 	m_pRenderTarget = NULL;
-	m_pLightSlateGreyBrush = NULL;
-	m_pCornFlowerBlueBrush = NULL;
+	m_pSolidColorBrush = NULL;
+	m_mModel.SetIdentity();
 }
 
 GFXDevice::~GFXDevice()
@@ -41,17 +42,9 @@ bool GFXDevice::CreateDeviceResources(HWND hWnd)
 		return false;
 	}
 
-	ASSERT(m_pLightSlateGreyBrush == NULL);
-	ASSERT(m_pCornFlowerBlueBrush == NULL);
-
-	hr = m_pRenderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::LightSlateGray), &m_pLightSlateGreyBrush);
-
-	if(hr != S_OK)
-	{
-		return false;
-	}
-
-	hr = m_pRenderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::CornflowerBlue), &m_pCornFlowerBlueBrush);
+	ASSERT(m_pSolidColorBrush == NULL);
+	
+	hr = m_pRenderTarget->CreateSolidColorBrush(Color(Color::LightSlateGray), &m_pSolidColorBrush);
 
 	if(hr != S_OK)
 	{
@@ -75,9 +68,24 @@ void GFXDevice::OnResize(uint32 uWidth, uint32 uHeight)
 	}
 }
 
-void GFXDevice::OnRender()
+void GFXDevice::BeginDraw()
 {
 	ASSERT(m_pRenderTarget != NULL);
+	m_pRenderTarget->BeginDraw();
+	m_pRenderTarget->SetTransform(D2D1::Matrix3x2F::Identity());
+}
+
+void GFXDevice::EndDraw()
+{
+	ASSERT(m_pRenderTarget != NULL);
+	
+	m_pRenderTarget->EndDraw();
+}
+
+void GFXDevice::OnRender()
+{
+
+	/*ASSERT(m_pRenderTarget != NULL);
 
 	m_pRenderTarget->BeginDraw();
 	
@@ -93,7 +101,7 @@ void GFXDevice::OnRender()
 		m_pRenderTarget->DrawLine(
 			D2D1::Point2F(static_cast<FLOAT>(x), 0.0f),
 			D2D1::Point2F(static_cast<FLOAT>(x), size.height),
-			m_pLightSlateGreyBrush,
+			m_pSolidColorBrush,
 			0.5f
 			);
 	}
@@ -103,7 +111,7 @@ void GFXDevice::OnRender()
 		m_pRenderTarget->DrawLine(
 			D2D1::Point2F(0.0f, static_cast<FLOAT>(y)),
 			D2D1::Point2F(size.height, static_cast<FLOAT>(y)),
-			m_pLightSlateGreyBrush,
+			m_pSolidColorBrush,
 			0.5f
 			);
 	}
@@ -122,7 +130,7 @@ void GFXDevice::OnRender()
 		size.height * 0.5f + 100.0f
 		);
 
-	m_pRenderTarget->FillRectangle(rect1, m_pLightSlateGreyBrush);
+	m_pRenderTarget->FillRectangle(rect1, m_pSolidColorBrush);
 	
 	const float fSpeed = 1.0f;
 	static float fX = 10.0f;
@@ -134,11 +142,17 @@ void GFXDevice::OnRender()
 	if(::GetAsyncKeyState('D')) fX += fSpeed;
 
 	m_pRenderTarget->SetTransform(D2D1::Matrix3x2F::Translation(fX, fY));
-	m_pRenderTarget->DrawRectangle(rect2, m_pCornFlowerBlueBrush);
+	m_pRenderTarget->DrawRectangle(rect2, m_pSolidColorBrush);
 
 	m_pRenderTarget->SetTransform(D2D1::Matrix3x2F::Identity());
 
-	m_pRenderTarget->EndDraw();
+	m_pRenderTarget->EndDraw();*/
+}
+
+void GFXDevice::Clear(const Color& color)
+{
+	ASSERT(m_pRenderTarget != NULL);
+	m_pRenderTarget->Clear(color);
 }
 
 void GFXDevice::DiscardDeviceResources()
@@ -147,6 +161,52 @@ void GFXDevice::DiscardDeviceResources()
 	SafeRelease(&m_pRenderTarget);
 }
 
+void GFXDevice::SetColor(const Color& color)
+{
+	ASSERT(m_pSolidColorBrush != NULL);
+	m_pSolidColorBrush->SetColor(&color);
+}
 
+void GFXDevice::DrawRect(const Vec2& vPos, const Vec2& vSize, const Color& color)
+{
+	ASSERT(m_pRenderTarget != NULL);
+	ASSERT(m_pSolidColorBrush != NULL);
+
+	m_pSolidColorBrush->SetColor(&color);
+	Vec2 vHalfSize = vSize * 0.5f;
+
+	Mat3x2 mM;
+	mM.SetIdentity();
+	mM.SetTranslation(-vSize.m_fX + vPos.m_fX - vHalfSize.m_fX, -vSize.m_fY + vPos.m_fY - vHalfSize.m_fY);
+
+	m_pRenderTarget->SetTransform(m_mModel * mM);
+
+	D2D1_RECT_F rect = D2D1::RectF
+		(
+			vSize.m_fX,
+			vSize.m_fY,
+			(vSize.m_fX * 2.0f),
+			(vSize.m_fY * 2.0f)
+		);
+	m_pRenderTarget->FillRectangle(rect, m_pSolidColorBrush);
+}
+
+void GFXDevice::DrawLine(float fXFrom, float fYFrom, float fXTo, float fYTo, const Color& color)
+{
+	ASSERT(m_pRenderTarget != NULL);
+	ASSERT(m_pSolidColorBrush != NULL);
+	
+	m_pSolidColorBrush->SetColor(&color);
+
+	D2D_POINT_2F from = { fXFrom, fYFrom };
+	D2D_POINT_2F to = { fXTo, fYTo };
+
+	m_pRenderTarget->DrawLine(from, to, m_pSolidColorBrush);
+}
+
+void GFXDevice::DrawLine(const Vec2& vFrom, const Vec2& vTo, const Color& color)
+{
+	DrawLine(vFrom.m_fX, vFrom.m_fY, vTo.m_fX, vTo.m_fY, color);
+}
 
 }	//	namespace neko
--- Projects/Engine/Graphics/GFXDevice.h
+++ Projects/Engine/Graphics/GFXDevice.h
@@ -5,6 +5,9 @@
 
 #pragma once
 #include "Engine/Common/Common.h"
+#include "Engine/Graphics/Color.h"
+#include "Engine/Math/Mat3x2.h"
+#include "Engine/Math/Vec2.h"
 
 namespace neko
 {
@@ -22,13 +25,26 @@ public:
 	void OnResize(uint32 uWidth, uint32 uHeight);
 	void OnRender();
 
+	void BeginDraw();
+	void EndDraw();
+
+	void Clear(const Color& color);
+	void SetColor(const Color& color);
+
+	void DrawRect(const Vec2& vPos, const Vec2& vSize, const Color& color);
+	void DrawLine(float fXFrom, float fYFrom, float fXTo, float fYTo, const Color& color);
+	void DrawLine(const Vec2& vFrom, const Vec2& vTo, const Color& color);
+
+	void SetModelMatrix(const Mat3x2& mModel) { m_mModel = mModel; }
+
 private:
 	void DiscardDeviceResources();
 
+	Mat3x2 m_mModel;
+
 	ID2D1Factory* m_pFactory;
 	ID2D1HwndRenderTarget* m_pRenderTarget;
-	ID2D1SolidColorBrush* m_pLightSlateGreyBrush;
-	ID2D1SolidColorBrush* m_pCornFlowerBlueBrush;
+	ID2D1SolidColorBrush* m_pSolidColorBrush;
 };
 
 }	//	namespace neko
