e94ebbb28c4be54fdc91806d1bad1233ff314fab
Olof Wallentin <olof@vitei.com>
2015-07-23 12:47:19 +0900

    Added String class
    Currently empty

---
+++ Projects/Engine/Core/String.h
@@ -0,0 +1,21 @@
+/****************************************************************************
+//	Filename: String.h
+//	Description: String class for managing text etc
+*****************************************************************************/
+#pragma once
+#include "Engine/Common/Common.h"
+
+namespace neko
+{
+
+template <typename ElemType>
+class IString
+{
+public:
+	IString(){}
+	~IString(){}
+private:
+
+};
+
+}	//	namespace neko
