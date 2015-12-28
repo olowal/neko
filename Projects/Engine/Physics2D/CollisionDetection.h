56405c61c265f8009d8eb7c8b610020451bc912b
Olof Wallentin <olov.wallentin@gmail.com>
2015-12-28 16:29:01 +0900

    Implemented various collision detection functions.

--- Projects/Engine/Physics2D/CollisionDetection.h
+++ Projects/Engine/Physics2D/CollisionDetection.h
@@ -5,7 +5,10 @@
 #pragma once
 #include "Engine/Common/Common.h"
 #include "Engine/Physics2D/CollisionShape.h"
+#include "Engine/Math/Utils.h"
 namespace neko
+{
+namespace collision
 {
 inline bool PointInBox(const BoxShape2D& box, const Vec2& point)
 {
@@ -20,6 +23,11 @@ inline bool PointInBox(const BoxShape2D& box, const Vec2& point)
 	return true;
 }
 
+inline bool PointInCircle(const CircleShape2D& circle, const Vec2& point)
+{
+	return point.Distance(circle.GetPosition()) < circle.shape.radius;
+}
+
 inline bool CircleToCircle(const CircleShape2D& a, const CircleShape2D& b)
 {
 	return ((a.GetPosition().Distance(b.GetPosition())) < (a.shape.radius + b.shape.radius));
@@ -27,6 +35,40 @@ inline bool CircleToCircle(const CircleShape2D& a, const CircleShape2D& b)
 
 inline bool CircleToBox(const CircleShape2D& circle, const BoxShape2D& box)
 {
-	return false;
+	const Vec2 circlePos = circle.GetPosition();
+	const Vec2 boxPos = box.GetPosition();
+	const Vec2 boxExtents = box.shape.extents;
+
+	const float boxLeft = boxPos.m_fX;
+	const float boxRight = boxPos.m_fX + boxExtents.m_fX;
+	const float boxTop = boxPos.m_fY;
+	const float boxBottom = boxPos.m_fY + boxExtents.m_fY;
+
+	const float closestX = math::Clamp(circlePos.m_fX, boxLeft, boxRight);
+	const float closestY = math::Clamp(circlePos.m_fY, boxBottom, boxTop);
+
+	const float distanceX = circlePos.m_fX - closestX;
+	const float distanceY = circlePos.m_fY - closestY;
+
+	const float distanceSq = (distanceX * distanceX) + (distanceY * distanceX);
+
+	return distanceSq < (circle.shape.radius * circle.shape.radius);
 }
+
+inline bool BoxToBox(const BoxShape2D& a, const BoxShape2D& b)
+{
+	const Vec2 aPos = a.GetPosition();
+	const Vec2 bPos = b.GetPosition();
+	
+	const Vec2 aExt = a.shape.extents;
+	const Vec2 bExt = b.shape.extents;
+
+	if((aPos.m_fX + aExt.m_fX) < bPos.m_fX) { return false; }
+	if(aPos.m_fX > (bPos.m_fX + bExt.m_fX)) { return false; }
+	if((aPos.m_fY + aExt.m_fY) < bPos.m_fY) { return false; }
+	if(aPos.m_fY > (bPos.m_fY + bExt.m_fY)) { return false; }
+
+	return true;
+}
+}	//	namespace collision
 }	//	namespace neko
--- Projects/Engine/Physics2D/CollisionPairs.cpp
+++ Projects/Engine/Physics2D/CollisionPairs.cpp
@@ -1,19 +1,31 @@
 //	CollisionPairs.cpp
 #include "CollisionPairs.h"
+#include "Engine/Physics2D/CollisionDetection.h"
 
 namespace neko
 {
+
 template <>
 bool CircleToCircleCollision2D::Intersects() const
 {
-	return ((a.GetPosition().Distance(b.GetPosition())) < (a.shape.radius + b.shape.radius));
+	return collision::CircleToCircle(a, b);
 }
+
 template <>
 bool CircleToBoxCollision2D::Intersects() const
 {
-	const float radius = a.shape.radius;
-	Vec2 max(radius, radius);
-	Vec2 min;
-	return true;
+	return collision::CircleToBox(a, b);
+}
+
+template <>
+bool BoxToCircleCollision2D::Intersects() const
+{
+	return collision::CircleToBox(b, a);
+}
+
+template <>
+bool BoxToBoxCollision2D::Intersects() const
+{
+	return collision::BoxToBox(a, b);
 }
 }	//	namespace neko
