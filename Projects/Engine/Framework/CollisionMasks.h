29272897afeb4abea178b3b5a9a4aea9e74df69c
Olof Wallentin <olov.wallentin@gmail.com>
2015-12-28 16:27:50 +0900

    Added CollisionMasks component.

---
+++ Projects/Engine/Framework/CollisionMasks.h
@@ -0,0 +1,14 @@
+/****************************************************************************
+//	Filename: CollisionMasks.h
+//	Description: A component containing data for collision masking
+*****************************************************************************/
+#pragma once
+#include "Engine/Common/Common.h"
+namespace neko
+{
+struct CollisionMasks
+{
+	uint32 masks;
+	uint32 targets;
+};
+}	//	namespace neko
