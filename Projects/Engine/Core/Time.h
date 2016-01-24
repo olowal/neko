20c3d2c2c21e6a3cd9bed6aad6802f98c94167b4
Olof Wallentin <olov.wallentin@gmail.com>
2016-01-25 00:27:30 +0100

    Cross-platform static time.
    Currently for windows only.

---
+++ Projects/Engine/Core/Time.h
@@ -0,0 +1,9 @@
+/****************************************************************************
+//	Filename: FileSystem.h
+//	Description: Platform specific includes for time operations
+*****************************************************************************/
+#pragma once
+#include "Engine/Common/Common.h"
+#ifdef PLATFORM_PC
+#include "Engine/Core/_win/Time.h"
+#endif
