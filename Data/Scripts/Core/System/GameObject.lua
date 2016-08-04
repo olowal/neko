f5d2d9a0705e5674623bb46c945210dcfb592fae
Olof Wallentin <olov.wallentin@gmail.com>
2016-08-04 12:01:02 +0200

    Implemented GameObject wrappers in lua.

--- Data/Scripts/Core/System/GameObject.lua
+++ Data/Scripts/Core/System/GameObject.lua
@@ -1,2 +1,27 @@
 --	GameObject.lua
---	Utility functions 
+--	Utility functions for managing game objects. GameObject is an int (index) which is from the engine. The class GameObject is created from the engine before compiling this chunk.
+
+GameObject = {}
+GameObject._gameObjects = {}
+
+local _init_game_object = function( index, parent )
+	local gameObject = {}
+	gameObject._components = {}
+	gameObject._index = index
+	gameObject._parent = parent or false
+	return gameObject
+end
+
+GameObject.Create = function()
+	local index = _GameObject.Create()
+	local gameObject = _init_game_object( index )
+	table.insert(GameObject._gameObjects, gameObject)
+	return gameObject
+end
+
+GameObject.CreateChild = function( parent )
+	local index = _GameObject.CreateChild( parent._index )
+	local gameObject = _init_game_object( index, parent )
+	table.insert(GameObject._gameObjects, gameObject)
+	return gameObject
+end
