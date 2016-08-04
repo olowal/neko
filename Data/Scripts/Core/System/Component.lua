8519e04196fccad4ca0f9193398ac371d9c00e5b
Olof Wallentin <olov.wallentin@gmail.com>
2016-08-04 12:12:21 +0200

    Implementation of Component wrapper functions for lua.

--- Data/Scripts/Core/System/Component.lua
+++ Data/Scripts/Core/System/Component.lua
@@ -4,9 +4,32 @@
 Component = {}
 
 Component.Get = function( component, gameObject )
-
+	return gameObject._components[component]
 end
 
 Component.GetFromParent = function( component, gameObject )
+	if gameObject._parent then
+		return gameObject._parent._components[component]
+	end
+	return nil
+end
 
+Component.GetFromParentRecursive = function( component, gameObject )
+	local c = nil
+	if gameObject._parent then
+		c = gameObject._parent._components[component]
+		if c == nil then
+			c = Component.GetFromParentRecursive( component, gameObject._parent)
+		end
+	end
+	return c
+end
+
+Component.Create = function( component, gameObject )
+	if gameObject._components[component] then
+		return false
+	end
+	local c = component.Alloc()
+	gameObject._components[component] = c
+	return true
 end
