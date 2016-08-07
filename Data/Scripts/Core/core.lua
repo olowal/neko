42f701df5c07a7f531410f15f6c05c3c1da057ab
Olof Wallentin <olov.wallentin@gmail.com>
2016-08-07 16:56:55 +0200

    Updated lua core compilation.

--- Data/Scripts/Core/core.lua
+++ Data/Scripts/Core/core.lua
@@ -4,6 +4,9 @@
 package.path = ";" .. Application.GetPath() .. "/?.lua;" .. package.path
 
 require "Scripts/Core/System/class"
+require "Scripts/Core/System/Priority"
+require "Scripts/Core/System/System"
 require "Scripts/Core/System/MessageSystem"
 require "Scripts/Core/System/Component"
+require "Scripts/Core/System/GameObject"
 require "Scripts/Core/Math/Vec2"
